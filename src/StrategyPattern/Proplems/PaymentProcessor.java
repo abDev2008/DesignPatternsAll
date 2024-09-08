24e9a46457d443695413e6cbd21ff722d809da51
Abel h <abelhailu320@gmail.com>
2024-09-08 01:10:34 -0500

    Strategy Pattern Problems

--- src/StrategyPattern/Proplems/PaymentProcessor.java
+++ src/StrategyPattern/Proplems/PaymentProcessor.java
@@ -1,2 +1,18 @@
-package StrategyPattern.Proplems;public class PaymentProcessor {
+package StrategyPattern.Proplems;
+
+public class PaymentProcessor {
+    public void processPayment(String type,double amount){
+        if(type.equals("cash")){
+            System.out.println("processing cash");
+        }
+        else if(type.equals("creditCard")){
+            System.out.println("processing credit card");
+        }
+        else if(type.equals("debitCard")){
+            System.out.println("processing debit card");
+        }
+        else if(type.equals("mobile")){
+            System.out.println("processing mobile");
+        }
+    }
 }
--- src/StrategyPattern/Proplems/Sorter.java
+++ src/StrategyPattern/Proplems/Sorter.java
@@ -1,2 +1,14 @@
-package StrategyPattern.Proplems;public class Sorter {
+package StrategyPattern.Proplems;
+
+import java.util.List;
+
+public class Sorter {
+    public void sort(List<Integer> list, String algorithm){
+        if(algorithm.equals("quickSort")){
+            System.out.println("performing quick sort");
+        }
+        else if(algorithm.equals("mergeSort")){
+            System.out.println("performing merge sort");
+        }
+    }
 }
