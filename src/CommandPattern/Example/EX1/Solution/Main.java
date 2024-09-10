0c4f7c343a34900327e9baaaa90966a7531c03a1
Abel h <abelhailu320@gmail.com>
2024-09-10 16:00:16 -0500

    refactor

--- src/CommandPattern/Example/EX1/Solution/Main.java
+++ src/CommandPattern/Example/EX1/Solution/Main.java
@@ -5,6 +5,7 @@ public class Main {
         TextEditor1 textEditor1 = new TextEditor1();
         Command writeHello = new WriteTextCommand(textEditor1,"hello world");
 
+
         CommandManager manager = new CommandManager();
         manager.executeCommand(writeHello);
         manager.executeCommand(writeHello);
